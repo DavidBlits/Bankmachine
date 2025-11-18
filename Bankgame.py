953cc606b26dc19f432aac5b8c0e4e2714f4984e
DavidBlits <blitsdavid@gmail.com>
2025-11-17 21:59:56 -0500

    Added colors

---
+++ Bankgame.py
@@ -0,0 +1,54 @@
+import random
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+RESET = '\033[0m'
+while True:
+    accepted={"swindrunner":"1234",'istormrage':'abcd','tpgallywix':'qwerty'}
+    user=input("Input username:")
+    password=input("Input password:")
+    if user in accepted.keys() and accepted[user]==password:
+        print(f'{GREEN}Correct login!{RESET}')
+        break
+    else:
+        print(f'{RED}Incorrect login, try again{RESET}')
+balance=random.randint(500,2500)
+while True:
+    action=input(f"Enter number, {BLUE}1:Display Balance{RESET}, {YELLOW}2:Withdraw{RESET}, {CYAN}3:Deposit{RESET}, {RED}4:Exit{RESET}")
+    #show balance
+    if action=='1':
+        print('Your balance:'+str(balance))
+    #Withdraw assuming the fee applies for the up to -500 rule
+    elif action=='2':
+        num=input('Enter withdraw amount:')
+        num=int(num)
+        if balance-num>0:
+            print("Your new balance is:"+str(balance-num))
+            balance=balance-num
+        #if balance becomes negative due to withdraw
+        elif 0>((balance-num)+(balance-num)*0.2):
+            if -500<((balance-num)+(num*0.2)):
+                choice=input(f'Are you sure? Balance will be less then {RED}zero{RESET}, resulting in 20 percent fee: {GREEN}Yes{RESET}/{RED}No{RESET}  ')
+            elif choice=="Yes" or choice=='yes' or choice=='y' or choice=='Y':
+                print("Your new balance is:"+str(round((balance-num)+(num*0.2)))+' with the fee amount taken')
+                balance=round((balance-num)+(num*0.2))
+            elif choice=='No' or choice=='n' or choice=='N' or choice=='no':
+                print('Choose another option.')
+        elif -500>(balance-num)+(num*0.2):
+            print("Unable to withdraw that much, balance will be over -500")
+    #Deposit
+    elif action =='3':
+        num2=int(input('How much are you depositing?'))
+        print('Your new balance is:'+str(balance+num2))
+        balance+=num2
+    #Exit
+    elif action=='4':
+        print(f"{GREEN}Thank you for using our bank!{RESET}")
+        break
+
